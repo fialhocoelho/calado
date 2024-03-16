0ed74a7ef7e4df98faefd572582dc6a46f46bf0d
fialhocoelho <jefferson@fialhocoelho.com.br>
2024-03-15 23:14:48 -0300

    add initial files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Experimentação de modelos para gerar medidas de calado com base em séries temporais',
+    author='Jefferson Fialho',
+    license='MIT',
+)
